6a5d007dc8cd4d36d6026009cfc45bea6a894e5f
Tim Miller <innerlogic4321@gmail.com>
2023-05-02 20:49:26 +0900

    Update Sample

---
+++ samples/Drastic.AudioRecorderMaui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Drastic.AudioRecorderMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ samples/Drastic.AudioRecorderMaui/Platforms/Android/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using AndroidX.Core.App;
+using AndroidX.Core.Content;
+
+namespace Drastic.AudioRecorderMaui;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        
+        if (ContextCompat.CheckSelfPermission (this, Manifest.Permission.RecordAudio) != Permission.Granted)
+        {
+            ActivityCompat.RequestPermissions (this, new String []{ Manifest.Permission.RecordAudio }, 1);
+        }
+    }
+}
